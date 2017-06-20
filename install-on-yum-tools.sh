397c5ed240e10d7cec6f49bc0a455996aa444654
Daniel Eklund <doeklund@gmail.com>
2017-06-20 09:41:08 -0400

    new

--- install-on-yum-tools.sh
+++ install-on-yum-tools.sh
@@ -5,4 +5,4 @@ sudo yum install -y git
 sudo yum install -y curl
 
 curl -L https://github.com/reverendpaco/vimrc/archive/master.tar.gz | tar xzv && mv vimrc-master .vim
-curl -L  https://github.com/AlgorexHealth/install-scripts/archive/master.tar.gz | tar xzv && mv install-scripts-master/.tmu* .. && mv install-scripts-master/tmu* ..
+curl -L  https://github.com/AlgorexHealth/install-scripts/archive/master.tar.gz | tar xzv && mv install-scripts-master/.tmu* . && mv install-scripts-master/tmu* .
