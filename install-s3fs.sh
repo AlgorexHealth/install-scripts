3843d5ac18f40eec3c25b308d20937d9b9a23c7c
Daniel Eklund <doeklund@gmail.com>
2017-06-20 09:53:57 -0400

    new

--- install-s3fs.sh
+++ install-s3fs.sh
@@ -1,4 +1,4 @@
-yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel
+sudo yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel
 git clone https://github.com/s3fs-fuse/s3fs-fuse
 cd s3fs-fuse/
 ./autogen.sh
