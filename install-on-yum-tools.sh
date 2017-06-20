5e1e85e97c6500e72fce37b65ce6ca47f19de136
Daniel Eklund <doeklund@gmail.com>
2017-06-20 09:24:45 -0400

    new

--- install-on-yum-tools.sh
+++ install-on-yum-tools.sh
@@ -1,3 +1,7 @@
 sudo yum -y update
 yum install tree
 yum install tmux
+yum install git
+yum install curl
+
+curl -L https://github.com/reverendpaco/vimrc/archive/master.tar.gz | tar xzv && mv vimrc-master .vim
