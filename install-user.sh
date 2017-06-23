4348fd942192ef73471fe210124a048bc3011bcd
Daniel Eklund <doeklund@gmail.com>
2017-06-23 12:46:55 -0400

    theunhesuh

--- install-user.sh
+++ install-user.sh
@@ -1,12 +1,12 @@
 sudo adduser smdermo -G ftpgroup
 sudo moduser smdermo -g ftpgroup
-cd /home/smdermo
-mkdir .ssh
-chmod 700 .ssh
-ssh-keygen -t rsa  -b 4096 -f .ssh/smdermo.pem -N ''
-cat .ssh/smdermo.pem >> .ssh/authorized_keys
-chmod 600 .ssh/authorized_keys
-chown smdermo:smdermo -R .ssh
+sudo cd /home/smdermo
+sudo mkdir .ssh
+sudo chmod 700 .ssh
+sudo ssh-keygen -t rsa  -b 4096 -f .ssh/smdermo.pem -N ''
+sudo cat .ssh/smdermo.pem >> .ssh/authorized_keys
+sudo chmod 600 .ssh/authorized_keys
+sudo chown smdermo:smdermo -R .ssh
 
 mkdir -p /jail/smdermo/data
 sudo mount --bind /ftp /jail/smdermo/data
