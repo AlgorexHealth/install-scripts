1f7770a553c4302bcc9a0945d2b9ce4436d37f6f
Daniel Eklund <doeklund@gmail.com>
2017-06-23 13:41:16 -0400

    new

--- install-user.sh
+++ install-user.sh
@@ -1,12 +1,12 @@
-sudo adduser smdermo -G ftpgroup
-sudo usermod -g ftpgroup smdermo
-sudo mkdir /home/smdermo/.ssh
-sudo chmod 700 /home/smdermo/.ssh
-sudo ssh-keygen -t rsa  -b 4096 -f /home/smdermo/.ssh/smdermo.pem -N ''
-sudo sh -c 'cat /home/smdermo/.ssh/smdermo.pem.pub >> /home/smdermo/.ssh/authorized_keys'
-sudo chmod 600 /home/smdermo/.ssh/authorized_keys
-sudo chown smdermo:smdermo -R /home/smdermo/.ssh
+sudo adduser $1 -G ftpgroup
+sudo usermod -g ftpgroup $1
+sudo mkdir /home/$1/.ssh
+sudo chmod 700 /home/$1/.ssh
+sudo ssh-keygen -t rsa  -b 4096 -f /home/$1/.ssh/$1.pem -N ''
+sudo sh -c 'cat /home/$1/.ssh/$1.pem.pub >> /home/$1/.ssh/authorized_keys'
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown $1:$1 -R /home/$1/.ssh
 
-sudo mkdir -p /jail/smdermo/data
-sudo chown concierge:ftpgroup /jail/smdermo/data
-sudo mount --bind /ftp /jail/smdermo/data
+sudo mkdir -p /jail/$1/data
+sudo chown concierge:ftpgroup /jail/$1/data
+sudo mount --bind /ftp /jail/$1/data
