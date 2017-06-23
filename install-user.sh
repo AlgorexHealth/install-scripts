9de80e9c779bd953b44c6815e73fb43c88cbcfea
Daniel Eklund <doeklund@gmail.com>
2017-06-23 13:04:23 -0400

    new

--- install-user.sh
+++ install-user.sh
@@ -3,9 +3,10 @@ sudo usermod smdermo -g ftpgroup
 sudo mkdir /home/smdermo/.ssh
 sudo chmod 700 /home/smdermo/.ssh
 sudo ssh-keygen -t rsa  -b 4096 -f /home/smdermo/.ssh/smdermo.pem -N ''
-sudo cat /home/smdermo/.ssh/smdermo.pem.pub >> /home/smdermo/.ssh/authorized_keys
+sudo sh -c 'cat /home/smdermo/.ssh/smdermo.pem.pub >> /home/smdermo/.ssh/authorized_keys'
 sudo chmod 600 /home/smdermo/.ssh/authorized_keys
 sudo chown smdermo:smdermo -R /home/smdermo/.ssh
 
-mkdir -p /jail/smdermo/data
+sudo mkdir -p /jail/smdermo/data
+sudo chown smdermo:smdermo /jail/smdermo
 sudo mount --bind /ftp /jail/smdermo/data
