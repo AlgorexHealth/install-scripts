31538cac60e22aabcc611faa2d89752fce827acb
EC2 Default User <ec2-user@ip-172-31-18-145.us-gov-west-1.compute.internal>
2017-07-13 12:22:11 +0000

    removed bad lines

--- install-python3-on-yum.sh
+++ install-python3-on-yum.sh
@@ -4,11 +4,3 @@ sudo yum -y groupinstall development
 sudo yum -y install https://centos6.iuscommunity.org/ius-release.rpm
 sudo yum -y install python36u-pip
 sudo yum -y install python36u-devel
-
-   56  python3.6 -m venv demo
-   57  source demo/bin/activate
-   60  pip3 install -r requirements.txt 
-   62  jupyter nbextension enable --py --sys-prefix widgetsnbextension
-   63  jupyter contrib nbextension install --user
-
-   70  jupyter notebook --ip=0.0.0.0 --no-browser  --NotebookApp.token=''
